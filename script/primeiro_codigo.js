3e4b3b200f1f7e23e5a72e8aff69af4cc3b06e24
“Adria” <adriastephanie@hotmail.com>
2021-08-13 22:19:44 -0300

    test

--- script/primeiro_codigo.js
+++
@@ -1,6 +0,0 @@
-console.log("hello world!");
-var nome = "João";
-console.log("olá " + nome + " Seja bem vindo");
-console.log('Welcome to typescript');
-var meuSobrenome = 'Castro';
-console.log("Meu sobrenome é: " + meuSobrenome);
